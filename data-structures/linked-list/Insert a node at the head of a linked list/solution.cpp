919dd1ed35af2a9bcf5e7af3d734d4ac73151f58
Aksels Ledins <aksels.ledins@gmail.com>
2018-02-10 14:27:26 +0100

    (data-structures)(linked-list) Insert a node at the head of a Linked List | 2018.02.10 | Cpp

---
+++ data-structures/linked-list/Insert a node at the head of a linked list/solution.cpp
@@ -0,0 +1,20 @@
+/*
+  Insert Node at the end of a linked list
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* Insert(Node *head,int data)
+{
+  // create new node
+  Node *item = new Node();
+  item->data = data;
+  // link it to the rest of the list
+  item->next = head;
+
+  return item;
+}
