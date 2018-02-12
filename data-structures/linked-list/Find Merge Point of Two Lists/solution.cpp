400a7485b3b595acf062f320c8b142bbd2f95566
Aksels Ledins <aksels.ledins@gmail.com>
2018-02-12 14:08:41 +0100

    (data-structures)(linked-list) Find Merge Point of Two Lists | 2018.02.12 | Cpp

---
+++ data-structures/linked-list/Find Merge Point of Two Lists/solution.cpp
@@ -0,0 +1,30 @@
+/*
+   Find merge point of two linked lists
+   Node is defined as
+   struct Node
+   {
+       int data;
+       Node* next;
+   }
+*/
+
+int FindMergeNode(Node *headA, Node *headB) {
+  Node *a = headA;
+  Node *b = headB;
+
+  // at some point the pointers will collide
+  while (a != b) {
+    if (!a->next) {
+      a = headB;
+    } else {
+      a = a->next;
+    }
+
+    if (!b->next) {
+      b = headA;
+    } else {
+      b = b->next;
+    }
+  }
+  return a->data;
+}
