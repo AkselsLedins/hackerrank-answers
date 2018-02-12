379dbbc0483fed2f24c6cf739d603d24cc044855
Aksels Ledins <aksels.ledins@gmail.com>
2018-02-12 13:02:54 +0100

    (data-structures)(linked-list) Get Node Value | 2018.02.12 | Cpp

---
+++ data-structures/linked-list/Get Node Value/solution.cpp
@@ -0,0 +1,31 @@
+/*
+  Get Nth element from the end in a linked list of integers
+  Number of elements in the list will always be greater than N.
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+int GetNode(Node *head,int positionFromTail)
+{
+
+  int i = 0;
+  Node* result = head;
+
+  while (head->next) {
+    // we create a gap of positionFromTail between the two pointers
+    if (i < positionFromTail) {
+      i += 1;
+    } else {
+      result = result->next;
+    }
+
+    head = head->next;
+  }
+
+  // when we arrived a the tail of the chained list we ha ve a gap of
+  // positionFromTail from the end with the result pointer
+  return result->data;
+}
